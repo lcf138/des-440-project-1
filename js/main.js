e4600ab077a597a941ea4b001dd5ba658cda63af
Leah Faulkner <Faulkner000@live.missouristate.edu>
2017-10-04 18:55:01 -0500

    Adding animations, adjusted text, and removed heights

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 /* Anything here will be processed by the javascript interpreter when this file finishes loading. */
 
 
+function handleScroll(passedElement) {
+  var _passedElement = $(passedElement);
+  if ( _passedElement ) {
+    var offset = $(_passedElement).offset().top;
+    $('html, body').animate({scrollTop: offset});
+  }
+}
+
 
 $(document).ready(function(){
 /* Anything here will run when the entire HTML document has been processed by the browser */
+
 
 
 });
